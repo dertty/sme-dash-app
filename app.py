33c2d10210a4a4a69a6ac0ac7298019dcc1dc171
dertty <alex.iv.vad@gmail.com>
2020-03-11 23:03:42 +0300

    Demo version - first commit

---
+++ app.py
@@ -0,0 +1,309 @@
+# Import required libraries
+import pickle
+import copy
+import pathlib
+import dash
+import math
+import pandas as pd
+from dash.dependencies import Input, Output, State, ClientsideFunction
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+import dash_daq as daq
+from data_preparation import loan_types, default_types, signed_dt_filtration, defaults_types_dropdown, loans_types_dropdown, navbar_labels
+from data_preparation import graph_count_data
+from datetime import datetime as dt
+import plotly.graph_objs as go
+
+# get relative data folder
+PATH = pathlib.Path(__file__).parent
+DATA_PATH = PATH.joinpath("data").resolve()
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
+app.config.suppress_callback_exceptions = True
+
+########################################
+# Navigation Bar
+#######################################
+navbar = dbc.NavbarSimple(
+    brand="Портфель ММБ", brand_href="#",
+    color="dark", dark=True, fluid=True,
+)
+
+
+########################################
+# DR and count blocks
+#######################################
+
+def stat_card(value, desc, id):
+    return dbc.Card(dbc.CardBody([
+        html.Div(html.H5(value, className="text-dark",), id=id),
+        html.P(desc, className="text-muted",)
+    ]), className='border border-light')
+
+
+cards = dbc.CardDeck(
+    [
+        stat_card(0, 'Доля дефолтов', id='dr-block'),
+        stat_card(0, 'Количество заявок', id='count-block'),
+        stat_card(0, 'Количество дефолтов', id='def-count-block'),
+    ], id='stat-blocks',
+)
+
+
+########################################
+# Plotting popularity graph
+#######################################
+
+count_plot_switches = dbc.FormGroup([
+    dbc.Checklist(
+        options=[
+            {"label": "Дефолты",   "value": 'default'},
+            {"label": "Не дефолты", "value": 'healthy'},
+            {"label": "Разбить на продукты", "value": 'decompose'},
+        ],
+        value=['default', 'healthy'],
+        id="switches-inline-input-count-plot",
+        inline=True, switch=True,
+    )])
+
+
+graph_count = dbc.Row([
+    dbc.Col([
+        dbc.Row([
+            dbc.Col(html.H3("Количество клиентов/договоров"), ),
+            dbc.Col(count_plot_switches, width='auto'), ], ),
+        dcc.Graph(figure={"data": [{"x": graph_count_data['Дата'], "y": graph_count_data['Количество'], }, ], }, id='count-graph', ),
+    ], className='m-1', ), ])
+
+
+########################################
+# Plotting rating graph
+#######################################
+
+rating_plot_switches = dbc.FormGroup([
+    dbc.Checklist(
+        options=[
+            {"label": "Дефолты",   "value": 'default'},
+            {"label": "Не дефолты", "value": 'healthy'},
+            {"label": "Разбить на продукты", "value": 'decompose'},
+        ],
+        value=['default', 'healthy'],
+        id="switches-inline-input-count-plot",
+        inline=True, switch=True,
+    )])
+
+rating_graph_data = graph_count_data.groupby(['Рейтинг']).size()
+
+graph_rating = dbc.Row([
+    dbc.Col([
+        dbc.Row([
+            dbc.Col(html.H3("Рейтинги клиентов/договоров"), ),
+            dbc.Col(rating_plot_switches, width='auto'), ], ),
+        dcc.Graph(
+            figure=go.Figure(
+                data=[go.Bar(x=rating_graph_data.index.tolist(), y=rating_graph_data.tolist(), name='SF Zoo'), ],
+                layout=go.Layout(barmode='stack')), id='rating-graph', ),
+    ], className='m-1', ), ])
+
+
+
+dark_theme_switch = daq.ToggleSwitch(
+    id='light-dark-theme-toggle',
+    label=['Light', 'Dark'],
+    style={'width': '100px', 'margin': 'auto'},
+    value=False,
+)
+
+
+
+
+
+
+
+
+sidebar = dbc.Col(
+    [
+        html.Div(
+            [
+                html.H5("Фильтрация портфеля"),
+                dbc.Label(signed_dt_filtration['label'], html_for=signed_dt_filtration['html_for']),
+                dcc.DatePickerRange(id='date-picker-range', start_date=signed_dt_filtration['start_date'], end_date=signed_dt_filtration['end_date'], clearable=True, first_day_of_week=1, display_format='DD-MM-YYYY'),
+                dbc.Label(defaults_types_dropdown['label'], html_for=defaults_types_dropdown['html_for']),
+                dcc.Dropdown(options=defaults_types_dropdown['options'], value=defaults_types_dropdown['values'], id='default-types', multi=True),
+                dbc.Label(loans_types_dropdown['label'], html_for=loans_types_dropdown['html_for']),
+                dcc.Dropdown(options=loans_types_dropdown['options'], value=loans_types_dropdown['values'], id='loan-types', multi=True),
+            ], className='sticky-top',
+        ),
+        html.Hr(),
+        dark_theme_switch,
+    ], width=3,
+)
+
+app.layout = dbc.Container(
+    [
+        navbar,
+        html.Div(
+            [
+                html.Br(),
+                dbc.Row(
+                    [
+                        sidebar,
+                        dbc.Col(
+                            [
+                                cards,
+                                html.Br(),
+                                dbc.Card(
+                                    [
+                                        dbc.CardHeader(
+                                            [
+                                                dbc.Tabs(
+                                                    [
+                                                        dbc.Tab(label="Количество", tab_id="tab-1", labelClassName="text-success"),
+                                                        dbc.Tab(label="Рейтинги", tab_id="tab-2",),
+                                                    ], id="tabs", active_tab="tab-1",
+                                                ),
+                                            ]
+                                        ),
+                                        dbc.CardBody(html.P(id="card-content", className="card-text")),
+                                    ]
+                                ),
+                            ], width=9,
+                        )
+                    ], className='m-1',
+                ),
+            ], id='main-theme',
+        ),
+    ], fluid=True,
+)
+
+
+def get_filtred_tbl(tbl, start_date, end_date, checked_default_types, checked_loan_types):
+    if checked_loan_types:
+        tbl = tbl[tbl['Тип продукта'].isin(checked_loan_types)]
+    if checked_default_types:
+        tbl = tbl[tbl['Тип дефолта'].isin(checked_default_types + [None, ])]
+    if start_date:
+        tbl = tbl[tbl['Дата'] >= start_date]
+    if end_date:
+        tbl = tbl[tbl['Дата'] < end_date]
+    return tbl
+
+
+@app.callback(
+    Output('dr-block', 'children'),
+    [
+        Input('date-picker-range', 'start_date'),
+        Input('date-picker-range', 'end_date'),
+        Input('default-types', 'value'),
+        Input('loan-types', 'value')
+    ])
+def update_dr_stat_block(start_date, end_date, checked_default_types, checked_loan_types):
+    plot_data = get_filtred_tbl(graph_count_data, start_date, end_date, checked_default_types, checked_loan_types)
+    return html.H5('{}%'.format(round(plot_data['Дефолт'].sum() / plot_data.shape[0] * 100, 2)), className="text-dark",)
+
+
+@app.callback(
+    Output('count-block', 'children'),
+    [
+        Input('date-picker-range', 'start_date'),
+        Input('date-picker-range', 'end_date'),
+        Input('default-types', 'value'),
+        Input('loan-types', 'value')
+    ])
+def update_count_stat_block(start_date, end_date, checked_default_types, checked_loan_types):
+    plot_data = get_filtred_tbl(graph_count_data, start_date, end_date, checked_default_types, checked_loan_types)
+    return html.H5(str(plot_data.shape[0]), className="text-dark",)
+
+
+@app.callback(
+    Output('def-count-block', 'children'),
+    [
+        Input('date-picker-range', 'start_date'),
+        Input('date-picker-range', 'end_date'),
+        Input('default-types', 'value'),
+        Input('loan-types', 'value')
+    ])
+def update_dc_stat_block(start_date, end_date, checked_default_types, checked_loan_types):
+    plot_data = get_filtred_tbl(graph_count_data, start_date, end_date, checked_default_types, checked_loan_types)
+    return html.H5(str(plot_data['Дефолт'].sum()), className="text-dark",)
+
+@app.callback(
+    Output('count-graph', 'figure'),
+    [
+        Input('date-picker-range', 'start_date'),
+        Input('date-picker-range', 'end_date'),
+        Input("switches-inline-input-count-plot", "value"),
+        Input('default-types', 'value'),
+        Input('loan-types', 'value')
+    ])
+def update_count_graph(start_date, end_date, checkbox_flag, checked_default_types, checked_loan_types):
+    flag_def = []
+    if 'default' in checkbox_flag:
+        flag_def.append(1)
+    if 'healthy' in checkbox_flag:
+        flag_def.append(0)
+    plot_data = graph_count_data[graph_count_data['Дефолт'].isin(flag_def)]
+
+    plot_data = get_filtred_tbl(plot_data, start_date, end_date, checked_default_types, checked_loan_types)
+    res = []
+    if 'decompose' in checkbox_flag:
+        for t in plot_data['Тип продукта'].unique():
+            res.append({
+                'x': plot_data.loc[plot_data['Тип продукта'] == t, 'Дата'],
+                'y': plot_data.loc[plot_data['Тип продукта'] == t, 'Количество'],
+                'name': list(loan_types.keys())[list(loan_types.values()).index(t)]
+            })
+        return {"data": res}
+    else:
+        return {"data": [{"x": plot_data['Дата'], "y": plot_data['Количество'], 'name': 'Сумма выбранных типов'}]}
+
+
+@app.callback(
+    Output('main-theme', 'style'),
+    [Input('light-dark-theme-toggle', 'value')]
+)
+def change_bg(dark_theme):
+    if dark_theme:
+        return {'background-color': '#303030', 'color': '#f9f9f9'}
+    else:
+        return {'background-color': '#f9f9f9', 'color': 'black'}
+
+
+@app.callback(
+    Output("card-content", "children"),
+    [Input("tabs", "active_tab")])
+def tab_content(active_tab):
+    if active_tab == 'tab-1':
+        return graph_count
+    elif active_tab == 'tab-2':
+        return graph_rating
+    else:
+        return "Hello, where are we? I don't know {} tab.".format(active_tab)
+
+
+@app.callback(
+    Output("tabs", "children"),
+    [Input("tabs", "active_tab")])
+def tab_content(active_tab):
+
+
+    if active_tab == 'tab-1':
+        return [
+            dbc.Tab(label="Количество", tab_id="tab-1", id='count-tab', labelClassName="text-success"),
+            dbc.Tab(label="Рейтинги", tab_id="tab-2", id='rating-tab'),
+        ]
+    elif active_tab == 'tab-2':
+        return [
+            dbc.Tab(label="Количество", tab_id="tab-1", id='count-tab',),
+            dbc.Tab(label="Рейтинги", tab_id="tab-2", id='rating-tab', labelClassName="text-success"),
+        ]
+    else:
+        return [
+            dbc.Tab(label="Количество", tab_id="tab-1", id='count-tab'),
+            dbc.Tab(label="Рейтинги", tab_id="tab-2", id='rating-tab'),
+        ]
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
---
+++ data_preparation.py
@@ -0,0 +1,81 @@
+import numpy as np
+import pandas as pd
+from datetime import datetime as dt
+
+# Имеется табличка с портфелем:
+#     id, - уникальный id строки
+#     load_date, - дата загрузки данных
+#     agr_cred_id, - уникальный id договора не на срез, надеюсь он не меняется
+#     inn, - ИНН клиента на срез
+#     signed_dt, - дата подписания договора на срез
+#     credit_srok, - срок ссуды в днях на срез
+#     issued_sum, - сумма ссуды на срез
+#     product_type, тип ссуды на срез
+#     default, - флаг нахождение в дефолте на срез
+#     report_dt - дата среза, конец месяца
+#     rating, - рейтинг на дату среза
+#
+
+# pd_data = pd.DataFrame(
+#     np.random.rand(10*1000),
+#     columns = ['id', 'load_date', 'agr_cred_id', 'inn', 'signed_dt', 'credit_srok', 'issued_sum', 'product_type', 'default', 'report_dt'])
+
+navbar_labels = {'label': 'ОМОРК ММБ - Риск метрики портфеля'}
+default_types = {
+    'Просрочка 90+ 12 мес.': 'v1',
+    'Банкротство': 'v2',
+    'Ликвидация': 'v3',
+    'Заражение': 'v4',
+}
+loan_types = {
+    'Разовые кредиты': 'v1',
+    'Возобновляемая кредитная линия': 'v2',
+    'Невозобновляемая кредитная линия': 'v3',
+    'Гарантии': 'v4',
+}
+
+signed_dt_filtration = {
+    'start_date': dt(2015, 1, 1),
+    'end_date': dt(2018, 10, 1),
+    'label': 'Интервал дат подписания договора:',
+    'html_for': "Минимальная и максимальная дата подписания договора для фильтрации выборки"
+}
+
+defaults_types_dropdown = {
+    'options': [
+        {'label': 'Просрочка 90+ 12 мес.', 'value': default_types['Просрочка 90+ 12 мес.']},
+        {'label': 'Банкротство', 'value': default_types['Банкротство']},
+        {'label': 'Ликвидация', 'value': default_types['Ликвидация']},
+        {'label': 'Заражение', 'value': default_types['Заражение']}, ],
+    'values': [*default_types.values()],
+    'label': 'Виды дефолтов:',
+    'html_for': "Выберите типы дефолтов для фильтрации выборки"
+}
+
+loans_types_dropdown = {
+    'options': [
+        {'label': 'Разовые кредиты', 'value': loan_types['Разовые кредиты']},
+        {'label': 'Возобновляемая кредитная линия', 'value': loan_types['Возобновляемая кредитная линия']},
+        {'label': 'Невозобновляемая кредитная линия', 'value': loan_types['Невозобновляемая кредитная линия']},
+        {'label': 'Гарантии', 'value': loan_types['Гарантии']},],
+    'values': [*loan_types.values()],
+    'label': 'Типы ссуд:',
+    'html_for': "Выберите типы ссуд для фильтрации выборки"
+}
+
+dates = [
+    dt(2013, 1, 1), dt(2013, 2, 1), dt(2013, 3, 1), dt(2013, 4, 1), dt(2013, 5, 1), dt(2013, 6, 1), dt(2013, 7, 1), dt(2013, 8, 1), dt(2013, 9, 1), dt(2013, 10, 1), dt(2013, 11, 1), dt(2013, 12, 1),
+    dt(2014, 1, 1), dt(2013, 2, 1), dt(2013, 3, 1), dt(2013, 4, 1), dt(2013, 5, 1), dt(2013, 6, 1), dt(2013, 7, 1), dt(2013, 8, 1), dt(2013, 9, 1), dt(2013, 10, 1), dt(2013, 11, 1), dt(2013, 12, 1),
+    dt(2015, 1, 1), dt(2013, 2, 1), dt(2013, 3, 1), dt(2013, 4, 1), dt(2013, 5, 1), dt(2013, 6, 1), dt(2013, 7, 1), dt(2013, 8, 1), dt(2013, 9, 1), dt(2013, 10, 1), dt(2013, 11, 1), dt(2013, 12, 1),
+    dt(2016, 1, 1), dt(2013, 2, 1), dt(2013, 3, 1), dt(2013, 4, 1), dt(2013, 5, 1), dt(2013, 6, 1), dt(2013, 7, 1), dt(2013, 8, 1), dt(2013, 9, 1), dt(2013, 10, 1), dt(2013, 11, 1), dt(2013, 12, 1),
+    dt(2017, 1, 1), dt(2013, 2, 1), dt(2013, 3, 1), dt(2013, 4, 1), dt(2013, 5, 1), dt(2013, 6, 1), dt(2013, 7, 1), dt(2013, 8, 1), dt(2013, 9, 1), dt(2013, 10, 1), dt(2013, 11, 1), dt(2013, 12, 1),
+    dt(2018, 1, 1), dt(2013, 2, 1), dt(2013, 3, 1), dt(2013, 4, 1), dt(2013, 5, 1), dt(2013, 6, 1), dt(2013, 7, 1), dt(2013, 8, 1), dt(2013, 9, 1), dt(2013, 10, 1), dt(2013, 11, 1), dt(2013, 12, 1),
+    dt(2019, 1, 1), dt(2013, 2, 1), dt(2013, 3, 1), dt(2013, 4, 1), dt(2013, 5, 1), dt(2013, 6, 1), dt(2013, 7, 1), dt(2013, 8, 1), dt(2013, 9, 1), dt(2013, 10, 1), dt(2013, 11, 1), dt(2013, 12, 1),
+    dt(2020, 1, 1), dt(2013, 2, 1), dt(2013, 3, 1), dt(2013, 4, 1), dt(2013, 5, 1), dt(2013, 6, 1), dt(2013, 7, 1), dt(2013, 8, 1), dt(2013, 9, 1), dt(2013, 10, 1), dt(2013, 11, 1), dt(2013, 12, 1),
+]
+
+graph_count_data = pd.DataFrame(index=pd.MultiIndex.from_product([loan_types.values(), default_types.values(), dates], names=["Тип продукта", 'Тип дефолта', "Дата"])).reset_index().sort_values('Дата')
+graph_count_data['Количество'] = [np.random.rand() for x in range(graph_count_data.shape[0])]
+graph_count_data['Дефолт'] = [int(np.random.rand() * 100) % 2 for x in range(graph_count_data.shape[0])]
+graph_count_data['Тип дефолта'] = graph_count_data[['Тип дефолта', 'Дефолт']].apply(lambda x: None if (x[1] == 0) else x[0], axis=1)
+graph_count_data['Рейтинг'] = [int(np.random.normal()) for x in range(graph_count_data.shape[0])]
